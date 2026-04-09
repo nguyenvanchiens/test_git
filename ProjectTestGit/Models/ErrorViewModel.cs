2772f5860982051618c9a4a9557ca45586548558
ChienNV <chiennv1@hncjsc.vn>
2026-04-09 08:59:47 +0700

    giỏi tạo dự án

---
+++ ProjectTestGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectTestGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
