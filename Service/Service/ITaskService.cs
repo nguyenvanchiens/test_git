7c9aac459908a73e6c202ff59f1db61af0922cad
ChienNV <chiennv1@hncjsc.vn>
2026-04-23 14:25:44 +0700

    test

---
+++ Service/Service/ITaskService.cs
@@ -0,0 +1,11 @@
+namespace Service.Service
+{
+    public interface ITaskService
+    {
+        string Hello();
+        string Hello2();
+        string Hello3();
+        string Hello4();
+        string Hello5();
+    }
+}
